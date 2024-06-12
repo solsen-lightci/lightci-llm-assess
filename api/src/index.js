efb01ec484a1a5489265ab1571c11749f253da0e
Steven Olsen <steven@serviceup.com>
2024-06-12 11:46:40 -0600

    move api into src folder

--- api/src/index.js
+++ api/src/index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = Number(process.env.PORT) || 3000;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "1mb" }));
 
 app.post("/messages", async (req, res) => {
   await new Promise((resolve) => setTimeout(resolve, 1000));
